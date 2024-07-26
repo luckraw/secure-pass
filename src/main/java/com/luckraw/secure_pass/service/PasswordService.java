431d28eab179ebe62c4566868b1dbc10fe38547f
Lucas <krawdev@gmail.com>
2024-07-26 19:44:50 -0300

    feat: service created.

---
+++ src/main/java/com/luckraw/secure_pass/service/PasswordService.java
@@ -0,0 +1,17 @@
+package com.luckraw.secure_pass.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PasswordService {
+
+    public List<String> validatePassword(String pass) {
+        List<String> failures = new ArrayList<>();
+
+
+        return failures;
+    }
+}
