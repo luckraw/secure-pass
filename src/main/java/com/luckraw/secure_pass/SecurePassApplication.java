4281abbcdff24952e1d43bf122250c4f18753412
Lucas <krawdev@gmail.com>
2024-07-26 19:24:32 -0300

    first commit

---
+++ src/main/java/com/luckraw/secure_pass/SecurePassApplication.java
@@ -0,0 +1,13 @@
+package com.luckraw.secure_pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurePassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurePassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckraw/secure_pass/SecurePassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckraw.secure_pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurePassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
